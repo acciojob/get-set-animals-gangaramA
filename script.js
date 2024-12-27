b210c90bb53ffdc6f94f6cf7e331fd43c42b31d8
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-12-27 19:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-//complete this code
-class Animal {}
+// Define the Animal class
+class Animal {
+    constructor(name, species) {
+        this.name = name;
+        this.species = species;
+    }
 
-class Dog extends Animal {}
+    sound() {
+        console.log(`${this.name} makes a sound.`);
+    }
+}
 
-class Cat extends Animal {}
+// Define the Dog class inheriting from Animal
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name, 'Dog');
+        this.breed = breed;
+    }
+
+    bark() {
+        console.log(`${this.name}, the ${this.breed}, barks loudly!`);
+    }
+}
+
+// Define the Cat class inheriting from Animal
+class Cat extends Animal {
+    constructor(name, color) {
+        super(name, 'Cat');
+        this.color = color;
+    }
+
+    meow() {
+        console.log(`${this.name}, the ${this.color} cat, meows softly.`);
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
